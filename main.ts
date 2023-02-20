5bd3213c0f9cb9438002fcaf3aa651b6f989512e
elvisodei <elvisodei124@gmail.com>
2023-02-20 05:09:03 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+let index = 0
+for (let index = 0; index <= index; index++) {
+    music.playTone(262, music.beat(BeatFraction.Quarter))
+}
+basic.showNumber(3 - index)
+music.playTone(392, music.beat(BeatFraction.Whole))
+basic.showString("\"Go!\"")
