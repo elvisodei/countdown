52328c3ce03bf83bd018d6d86a25764fe22d33a8
elvisodei <elvisodei124@gmail.com>
2023-02-20 05:12:42 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 let index = 0
-for (let index = 0; index <= index; index++) {
+for (let index = 0; index <= 2; index++) {
     music.playTone(262, music.beat(BeatFraction.Quarter))
 }
 basic.showNumber(3 - index)
